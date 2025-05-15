b82631831e37cd08536c00219d3c48fb21d9f3c6
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2025-05-15 21:32:16 +0500

    Align comments and add small clarification in the comments

--- boot/bios/include/32_bit_protected_mode/enter_long_mode/enter_long_mode.asm
+++ boot/bios/include/32_bit_protected_mode/enter_long_mode/enter_long_mode.asm
@@ -29,18 +29,20 @@ switch_to_lm_from_pm:
 [bits 64]
 
 realm64:
-    cli                           ; Clear the interrupt flag.
-    mov ax, GDT64.data            ; Set the A-register to the data descriptor.
-    mov ds, ax                    ; Set the data segment to the A-register.
-    mov es, ax                    ; Set the extra segment to the A-register.
-    mov fs, ax                    ; Set the F-segment to the A-register.
-    mov gs, ax                    ; Set the G-segment to the A-register.
-    mov ss, ax                    ; Set the stack segment to the A-register.
-    mov edi, 0xB8000              ; Set the destination index to 0xB8000 (the VGA (Video Graphics Array) address).
-    mov rax, 0x1F201F201F201F20   ; Set the A-register to 0x1F201F201F201F20.
-    mov ecx, 500                  ; Set the C-register to 500.
-    rep stosq                     ; Clear the screen.
-    hlt                           ; Halt the processor.
+    cli                             ; Clear the interrupt flag.
+    mov ax, GDT64.data              ; Set the A-register to the data descriptor.
+    mov ds, ax                      ; Set the data segment to the A-register.
+    mov es, ax                      ; Set the extra segment to the A-register.
+    mov fs, ax                      ; Set the F-segment to the A-register.
+    mov gs, ax                      ; Set the G-segment to the A-register.
+    mov ss, ax                      ; Set the stack segment to the A-register.
+    mov edi, 0xB8000                ; Set the destination index to 0xB8000 
+                                    ; (the VGA (Video Graphics Array) address).
+    mov rax, 0x1F201F201F201F20     ; Set the A-register to 0x1F201F201F201F20 
+                                    ; (white spaces with blue background).
+    mov ecx, 500                    ; Set the C-register to 500.
+    rep stosq                       ; Clear the screen.
+    hlt                             ; Halt the processor.
 
 ; ======================================== DEBUGGING MESSAGES ============================================
 SET_LM_BIT_MSG db "Set LM-bit.", 0
