b88555e7ec84c64cf0c26e7a9bbe8dd9611582f2
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2025-12-12 20:12:08 +0500

    [kernel]: Set up GDT and IDT descriptor tables and load them. - Create struct types and helper functions to set and load GDT and IDT. - Update the make config file to include new object files (should be   rewritten properly, I guess) - Write some assembly to implement interrupt handlers, load the GDT   and IDT, and set the segment registers.
    The interrupt handler doesn't work properly!!! But the OS boots and
    stays up :)

--- TODO.md
+++ TODO.md
@@ -1,4 +1,7 @@
 # TODO Tasks
+- Fix the issue with isr_handler function in isr.c file: too many repeated print messages and empty 
+  int_no printed.
+- Read [https://archive.is/8W6ew]
 
 # Optional or long term todo tasks
 - Write your own bootloader :)
--- kernel/arch/x86/kernel/kernel/kernel.c
+++ kernel/arch/x86/kernel/kernel/kernel.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/descriptor_tables.h>
 
 void kernel_main(void) {
+    init_descriptor_tables();
+
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+    printf("Welcome to Nurbo OS!!!");
+
+    asm volatile ("int $0x3");
+    asm volatile ("int $0x4"); 
 }
